2b82db410f9ebacbe4fb5967c604cad1c82b7078
RinatGaraev <153435700+RinatGaraev@users.noreply.github.com>
2023-12-10 21:33:10 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,565 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--1--",
+      "name": "Зона 1 технического обслуживания",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -0.28533504489945827,
+        "pitch": 0.13591265499957927,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6837363348663015,
+          "pitch": 0.059664202554156276,
+          "rotation": 0,
+          "target": "1--2--"
+        },
+        {
+          "yaw": 0.5413327032268853,
+          "pitch": 0.19424802947709097,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": 0.8018898295330974,
+          "pitch": -0.029816652215288286,
+          "rotation": 0,
+          "target": "8--"
+        },
+        {
+          "yaw": -0.26631609977186343,
+          "pitch": 0.2999526435720625,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9901289936688,
+          "pitch": -0.380151602542524,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Вытяжка отработавших газов</font></font>",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1--2--",
+      "name": "Зона 2 технического обслуживания",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 2.8710529972920487,
+        "pitch": 0.04129935683918262,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20051769491925953,
+          "pitch": -0.01652974201609858,
+          "rotation": 0,
+          "target": "0--1--"
+        },
+        {
+          "yaw": 0.9540607010386672,
+          "pitch": 0.2368817437611419,
+          "rotation": 0,
+          "target": "6--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--3--",
+      "name": "Зона 3 технического обслуживания",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6471583149687721,
+          "pitch": 0.7002490944160762,
+          "rotation": 0,
+          "target": "8--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0056408022081697595,
+          "pitch": 0.5778422958668656,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Канава смотровая</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 1.5742313220734374,
+          "pitch": 0.09364879292543371,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Установка для удаления отработанного масла</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 1.2950096177150439,
+          "pitch": -0.11597998322290337,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Стойка для снятия трансмиссии</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.9234995615396784,
+          "pitch": 0.3361412333839624,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Домкрат гидравлический</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.7356746010581308,
+          "pitch": -0.24097292472110077,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Шкаф для спецодежды</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.4859622330802722,
+          "pitch": -0.046040009000886783,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Пресс гидравлический</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.8579836497548694,
+          "pitch": -0.10056590260706244,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Станок балансировочный</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.2964183509398133,
+          "pitch": -0.0403884987157177,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Подъемник</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.2370067394328856,
+          "pitch": 0.2369974757804929,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Станок бортировочный</font></font>",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "3-----",
+      "name": "Установка для удаления отработанного масла",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.018233820814064217,
+        "pitch": -0.22082451937119707,
+        "fov": 1.1471403603698627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.33674891211484415,
+          "pitch": -0.4678063776178032,
+          "rotation": 0,
+          "target": "0--1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2589556235239492,
+          "pitch": -0.5098851096372972,
+          "title": "Установка для удаления отработанного масла<br>",
+          "text": "<span style=\"font-size: 14pt; line-height: 115%; font-family: &quot;Times New Roman&quot;, &quot;serif&quot;;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Для сбора самотеком моторного и трансмиссионного масла, а также других неагрессивных автомобильных жидкостей</font></font></span><br>"
+        }
+      ]
+    },
+    {
+      "id": "4--",
+      "name": "Установка маслозаправочная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": -0.031228555204670272,
+        "pitch": -0.13268635588978483,
+        "fov": 1.2061874717363192
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5220613832967906,
+          "pitch": -0.22514896895017422,
+          "rotation": 0,
+          "target": "0--1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5216114787229635,
+          "pitch": -0.5273503257065357,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Установка маслозаправочная</font></font>",
+          "text": "<span style=\"font-size: 14pt; line-height: 115%; font-family: &quot;Times New Roman&quot;, &quot;serif&quot;;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">рекомендуется для заправки масла</font></font></font></font></span><br>"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "Подъемник",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -0.047887194129481614,
+        "pitch": -0.02111321911408126,
+        "fov": 1.2061874717363192
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08038115272122326,
+          "pitch": -0.2999880543086313,
+          "rotation": 0,
+          "target": "0--1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1984320061960574,
+          "pitch": -0.3011714384469997,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Подъемник автомобильный</font></font>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 12pt; line-height: 115%;\">Четырехстоечный электрогидравлический подъемник для проведения\nслесарных работ<o:p></o:p></span></p>"
+        }
+      ]
+    },
+    {
+      "id": "6--",
+      "name": "Тележка инструментальная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 850,
+      "initialViewParameters": {
+        "yaw": 0.027064747844054438,
+        "pitch": -0.04824749886944879,
+        "fov": 1.2061874717363192
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8150701560011449,
+          "pitch": -0.12384781659297417,
+          "rotation": 0,
+          "target": "1--2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.814151322563184,
+          "pitch": -0.33599845517974813,
+          "title": "<span style=\"font-size: 12pt; line-height: 115%; font-family: &quot;Times New Roman&quot;, &quot;serif&quot;;\">Инструментальная тележка</span><br>",
+          "text": "<span style=\"font-size: 12pt; line-height: 115%; font-family: &quot;Times New Roman&quot;, &quot;serif&quot;;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Для хранения рабочего инструмента и инвентаря</font></font></font></font></font></font></span><br>"
+        }
+      ]
+    },
+    {
+      "id": "7-",
+      "name": "Моечная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -0.05202508245293558,
+        "pitch": 0.009079009303135877,
+        "fov": 1.2061874717363192
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3223633764554741,
+          "pitch": -0.11059347191017821,
+          "rotation": 0,
+          "target": "8--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.07239002414120677,
+          "pitch": 0.43314275916747924,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Слив жидкости</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.7671110412479436,
+          "pitch": 0.15446269274467106,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Пистолет моечный</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Холодная вода</font></font>"
+        },
+        {
+          "yaw": 0.7172780622719692,
+          "pitch": 0.22620879506577296,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Пистолет моечный</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Горячая вода</font></font>"
+        },
+        {
+          "yaw": 0.9436370075673324,
+          "pitch": 0.0998195008664613,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Зажимы для ковриков</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.6671709469743536,
+          "pitch": -0.5707622288103291,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Поворотные штанги</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.3572426687303718,
+          "pitch": -0.11055655755652438,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Емкость</font></font></font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Химия для мойки</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "8--",
+      "name": "Главный вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 650,
+      "initialViewParameters": {
+        "yaw": -0.10750109106109562,
+        "pitch": 0.30692332701394776,
+        "fov": 1.2061874717363192
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6233862608493119,
+          "pitch": 0.31033916882359236,
+          "rotation": 0,
+          "target": "0--1--"
+        },
+        {
+          "yaw": 0.1760763392134308,
+          "pitch": 0.3072920293889059,
+          "rotation": 0,
+          "target": "2--3--"
+        },
+        {
+          "yaw": -0.2846594853751565,
+          "pitch": 0.31812401265271895,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": -0.7806742886248124,
+          "pitch": 0.3154253463583423,
+          "rotation": 0,
+          "target": "9-----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-----",
+      "name": "Зона технического обслуживания грузовых автомобилей",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 700,
+      "initialViewParameters": {
+        "yaw": -0.10617708769588319,
+        "pitch": -0.000002574303143987322,
+        "fov": 1.2061874717363192
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2785529380174356,
+          "pitch": 0.10563900723791697,
+          "rotation": 0,
+          "target": "8--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3077551181050957,
+          "pitch": 0.14259624001823923,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Канава смотровая</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.205676604536439,
+          "pitch": 0.08874053468101906,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Кантователь для двигателя</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -0.9829885417117978,
+          "pitch": -0.1573685765833801,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Верстак слесарный</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.7256648146019593,
+          "pitch": 0.40203455010063394,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Оборудование для регулировки света фар</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.3135138952937311,
+          "pitch": 0.1283756428730065,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Тележка для снятия колес</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.6953212005755898,
+          "pitch": 0.022165637044274433,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Установка для удаления масла</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.2875099620612751,
+          "pitch": -0.18129243725887179,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Тележка инструментальная</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 1.170194453680896,
+          "pitch": -0.1426919601840524,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Домкрат гидравлический</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 0.9878383942776239,
+          "pitch": -0.07887119337828885,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Стойка для снятия трансмиссии</font></font>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.2228284067067499,
+          "pitch": -0.49166334344798557,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Вытяжка выхлопных газов</font></font>",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
